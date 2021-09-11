1f5fc2b530689500ef71d47e0b1312cf5e539b31
Hieu Ma <simshieu1@gmail.com>
2021-09-11 16:10:36 -0700

    added about-us component

--- pansuit/src/app/app.module.ts
+++ pansuit/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrandambFormComponent } from './brandamb-form/brandamb-form.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BrandambFormComponent
+    BrandambFormComponent,
+    AboutUsComponent
   ],
   imports: [
     BrowserModule,
